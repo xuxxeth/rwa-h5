7e0eb4ca9332f08a97a4a99aceb60f3d817d0371
Guowei <138768192+time-endless@users.noreply.github.com>
2026-04-17 16:11:18 +0800

    feat: save (#79)
    Co-authored-by: guowei <guoweihd@126.com>

--- src/components/compliance/index.tsx
+++ src/components/compliance/index.tsx
@@ -28,7 +28,12 @@ export const openExternal = (url: string) => (event: React.MouseEvent<HTMLAnchor
       navigator.userAgent
     )
   })()
+
+  const isAndroid = /android/i.test(navigator.userAgent);
   console.log('isMobile', isMobile)
+  console.log('isAndroid', isAndroid)
+  console.log('navigator.userAgent', navigator.userAgent)
+
   const isWalletWebView =
     /MetaMask|OKX|Binance|Trust|TokenPocket|imToken|Bitget|Bybit|CoinbaseWallet|Coin98|Rainbow|Phantom|SafePal|MathWallet|ONTO|1inch|Klever/i.test(
       navigator.userAgent
