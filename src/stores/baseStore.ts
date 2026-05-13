fc055bab8e9f8aaa825ba6ca84a4ac59e79b636a
Ton <yjxx0607@gmail.com>
2026-05-13 18:01:42 +0800

    feat: 恢复夜盘最后10分钟显示 (#109)

--- src/stores/baseStore.ts
+++ src/stores/baseStore.ts
@@ -150,7 +150,7 @@ export const useBaseStore = create<BaseStore>()(
         const res = await baseApi.getMarket()
         if (res.code === RESPONSE_CODE.SUCCESS) {
           const marketInfo = { ...(res.data || {}) }
-          set({ marketInfo: {...marketInfo, nightTradingEndTime: Math.max((marketInfo.nightTradingEndTime || 0) - 10 * 60 * 1000, 0)} })
+          set({ marketInfo: {...marketInfo} })
         }
         return res
       },
