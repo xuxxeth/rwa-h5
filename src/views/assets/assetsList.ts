e0bc8c8f8ed25fd52e10c0845e18ec8781204e9a
guowei <guoweihd@126.com>
2026-07-08 21:47:10 +0800

    feat: add useTokenBalances V2

--- src/views/assets/assetsList.ts
+++ src/views/assets/assetsList.ts
@@ -5,13 +5,8 @@ import { useBaseStore } from '@/stores/baseStore'
 import { useEffect, useState } from 'react'
 import wsService from '@/service/webSocket/service'
 import type { IAggregateData } from '@/service/webSocket/types'
-import { useRegulateAssets } from 'ca-common-web'
-
-function useDiamondContract(chainId: number) {
-  const chainList = useBaseStore(state => state.chainList)
-  const chain = chainList.find(chain => chain.id === chainId)
-  return chain?.contract
-}
+import { useRegulateAssets, type RegulateAssetItem } from 'ca-common-web'
+import { useAppStore } from '@/stores/appStore'
 
 export interface IRiskControlAsset {
   token: string
@@ -21,19 +16,32 @@ export interface IRiskControlAsset {
 }
 
 export function useRiskControlAssets(chainId: number, account: string): IRiskControlAsset[] {
+  const currentChainId = useAppStore(state => state.currentChainId)
+  const chainList = useBaseStore(state => state.chainList)
   const rwaList = useRwaTokens(false)
   const tokenList = useTokens()
-  const diamondContract = useDiamondContract(chainId)
+  const allTokens = [...tokenList, ...rwaList]
 
-  const { assets, isLoading, error, refetch } = useRegulateAssets(
-    diamondContract,
-    account,
-    [...rwaList, ...tokenList].map(rwa => rwa.address)
-  )
+  const { getRegulateAssets } = useRegulateAssets()
 
-  if (!assets) return []
+  const [assets, setAssets] = useState<RegulateAssetItem[]>([])
 
-  const allTokens = [...rwaList, ...tokenList]
+  useEffect(() => {
+    const filteredTokens = [...rwaList, ...tokenList].filter(
+      item => item.chainId === currentChainId
+    )
+    const diamondContractAddr =
+      chainList.find(chain => chain.id === currentChainId)?.contract ?? null
+    if (!diamondContractAddr || !account || !filteredTokens.length) return
+
+    getRegulateAssets(
+      diamondContractAddr,
+      account,
+      filteredTokens.map(rwa => rwa.address)
+    ).then(res => {
+      setAssets(res)
+    })
+  }, [currentChainId, account, rwaList, tokenList, chainList])
 
   return Object.entries(assets.filter(item => item.amount !== 0n))
     .map(([_, { token, amount }]) => {
